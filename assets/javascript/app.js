ee8cd1cf2ee5a82ec8e8c009601d23dad1563639
bryanbuckus <bryanbuckus@gmail.com>
2019-03-10 21:14:09 -0400

    added my new firebase link

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+
+
+var config = {
+    apiKey: "AIzaSyCENTyE5FAschXgGqJVIaBCDYpsjlLyin4",
+    authDomain: "train-scheduler-1872b.firebaseapp.com",
+    databaseURL: "https://train-scheduler-1872b.firebaseio.com",
+    projectId: "train-scheduler-1872b",
+    storageBucket: "train-scheduler-1872b.appspot.com",
+    messagingSenderId: "982199595729"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
